266f230a4a177c081ed3267a3639e9761822435b
Vincent Palomo <vincentpalomo@yahoo.com>
2022-12-14 20:09:41 -0600

    forgot to save singlepost.js

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const SinglePost = () => {
-  return (
-    <div className=''>
-      <h1>siglepost</h1>
-    </div>
-  );
+  return <div>SinglePost</div>;
 };
 
 export default SinglePost;
